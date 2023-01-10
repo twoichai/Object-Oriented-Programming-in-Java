7d39c81ffe82b736c74d5f4327b7d66d3e6d9706
Illya B <102254341+twoichai@users.noreply.github.com>
2023-01-10 08:52:00 +0100

    Update HW-2.java

--- HW-2.java
+++ HW-2.java
@@ -1,41 +1,43 @@
-public class Main {
+public String remove(String arg) {
+		
+		// our given values (strings)
+		String elementsToRemove = arg, stringToCompare = myString;
+		String elementsToRemoveLower = elementsToRemove.toLowerCase();
+		String elementsToRemoveUpper = elementsToRemove.toUpperCase();
 
-    public static void main(String[] args) {
-
-
-        String stringToCompare = "MMMMMmmmmmy lucky nUmbeRs are 7, 8, and 19.";  /* be used */
-        String elementsToRemove = "mr A.7";  /* be used */
-        String elementsToRemoveLower = elementsToRemove.toLowerCase();
-        String elementsToRemoveUpper = elementsToRemove.toUpperCase();
-        System.out.println(elementsToRemoveUpper);
-    
-        char[] charsToRemove = elementsToRemove.toCharArray();
-        char[] charsToCompare = stringToCompare.toCharArray();
-        char[] charsToRemoveUpper = elementsToRemoveUpper.toCharArray();    
-        char[] charsToRemoveLower = elementsToRemoveLower.toCharArray();
-        //Vergleiche für jeden Buchstaben, der entfernt werden soll
-        for(var i = 0; i < charsToRemove.length; i++) {
-            
-            //Erstelle ein neues Char-Array, um die nicht zu löschenden Werte zu speichern
-            //Initialisiere auf maximale Lönge des zu vergleichenden char-arrays
-            var newChars = new char[charsToCompare.length];
-            var newCharIndex = 0;
-            
-            //Vergleiche für jeden Buchstaben im Eingabe/Compare-Array, ob der Buchstabe gelöscht werden soll
-            for(var j = 0; j < charsToCompare.length; j++) {
-                // Sind die Werte nicht gleich, wird der Buchstabe kopiert
-                if(charsToRemoveLower[i] != charsToCompare[j] && charsToRemoveUpper[i] != charsToCompare[j] || (!Character.isLetter(charsToRemove[i]) == true) ) { //hot
-                //if(charsToRemove[i] != charsToCompare[j] && charsToRemoveUpper[i] != charsToCompare[j] || (!Character.isLetter(charsToRemove[i]) == true) )
-                    // wenn die Chars ungleich sind oder es ein symbol oder zahl ist, kopieren wir es in neue Array und wiederholen die loop
-                    newChars[newCharIndex] = charsToCompare[j];
-                    newCharIndex ++;
-                }
-                
-            }    
-            // Bevor ein neuer Durchlauf startet, setze das verkleinerte Array als Eingabe/Compare-Array
-            charsToCompare = newChars;    // will replace the one with the removed letter
-        } 
-        String result = new String(charsToCompare);
-        System.out.println(result);
-    }
-}
+		// make char arrays out of the input string myString and arg in lower and upper case
+		char[] charsToCompare = stringToCompare.toCharArray(), charsToRemoveUpper = elementsToRemoveUpper.toCharArray(), 
+				charsToRemoveLower = elementsToRemoveLower.toCharArray();
+		
+		// compare each letter, which should be removed
+		for(var i = 0; i < charsToRemoveUpper.length; i++) {
+			
+			// create a new char array to save the elements which should be kept
+			// give the char array the length of the string
+			var newChars = new char[charsToCompare.length];
+			var newCharIndex = 0;
+			
+			// creating a loop, to go through the arg first && compare each element from compare/remove array which should or not be copied
+			for(var j = 0; j < charsToCompare.length; j++) {
+				
+				//the value is not the same -> we copy the value to newChar array
+				if(charsToRemoveLower[i] != charsToCompare[j] && charsToRemoveUpper[i] != charsToCompare[j] || (!Character.isLetter(charsToRemoveUpper[i]) == true) ) { 
+					/* 1st: charToRemoveLower is not equal to charToCompare AND charsToRemoveUpper is not equal to charToCompare OR
+					it is something except letter ------only then------> we can copy it out and insert it to the newChar
+					*/ // -> case insensitive
+					
+					// then we do the copy:
+					// wenn die Chars ungleich sind oder es ein symbol oder zahl ist, kopieren wir es in neue Array und wiederholen die loop
+					newChars[newCharIndex] = charsToCompare[j];
+					
+					// going to the next placeholder of the newChar[]
+					newCharIndex ++;
+				}
+			}	
+			// going to the 1st loop again 
+			// before we start the 1st loop, which changes the index of the toRemove arrays, we replace the chars to compare with the newChars array
+			charsToCompare = newChars;	// will replace the one with the removed letter
+		} 
+		String result = new String(charsToCompare);		
+	  	return result;
+	}
